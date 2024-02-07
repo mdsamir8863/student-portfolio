6d6b40af43476f813552f088bbad9e97ead01338
mdsamir8863 <mentoonsdeveloper.ansari@gmail.com>
2024-02-07 14:44:38 +0530

    active-state-added

--- nav.js
+++ nav.js
@@ -4,3 +4,14 @@ const lists = document.querySelector(".lists");
 navIcon.addEventListener("click", function () {
   lists.classList.toggle("hideShow");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var currentLocation = window.location.href;
+  var navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(function (navLink) {
+    if (navLink.href === currentLocation) {
+      navLink.classList.add("active");
+    }
+  });
+});

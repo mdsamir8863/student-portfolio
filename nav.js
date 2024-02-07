93508193fa0ea358848a2dd65535ae6b97bebe72
mdsamir8863 <mentoonsdeveloper.ansari@gmail.com>
2024-02-07 15:24:57 +0530

    one feature added

--- nav.js
+++ nav.js
@@ -4,6 +4,26 @@ const lists = document.querySelector(".lists");
 navIcon.addEventListener("click", function () {
   lists.classList.toggle("hideShow");
 });
+const Myname = document.querySelector(".Myname");
+
+// Function to change the name
+function changeName() {
+  var names = ["My Name", "Developer", "Designer", "Coder"];
+  var randomIndex = Math.floor(Math.random() * names.length);
+  var newName = names[randomIndex];
+  var spanElement = document.getElementById("nameSpan");
+
+  if (spanElement.textContent !== newName) {
+    spanElement.style.animation = "none";
+    spanElement.offsetHeight;
+    spanElement.style.animation = null;
+  }
+
+  spanElement.textContent = newName;
+}
+
+// Change name every second
+setInterval(changeName, 1000);
 
 document.addEventListener("DOMContentLoaded", function () {
   var currentLocation = window.location.href;
